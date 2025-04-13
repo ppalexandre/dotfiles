a3a097038012859198f1d732cdfdb3ac1b9f8a4b
Pedro P. Alexandre <pedropalexandre@protonmail.com>
2025-04-12 21:15:55 -0300

    Adds my vimrc and updates my nvim configs

--- .config/nvim/lua/assorted_configs.lua
+++ .config/nvim/lua/assorted_configs.lua
@@ -1,3 +1,13 @@
+-- hightlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
+
 -- stops pasting in visual mode from yanking into the register 
 vim.keymap.set("v", "p", "P")
 
