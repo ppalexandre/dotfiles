ff96efb4c987b9b343155a065bca0e8f537f1ba2
Pedro P. Alexandre <pedropalexandre@protonmail.com>
2025-08-03 18:48:27 -0300

    Updates my configs

--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+WALLPAPER=""
 WALLPAPER_DIRECTORY=$HOME/.wallpapers
 WALLPAPER_LIST=$(find "$WALLPAPER_DIRECTORY"/* -type f | sort)
 WALLPAPER_LIST=($WALLPAPER_LIST)
 CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
-CURRENT_WALLPAPER=$(readlink -f $CURRENT_WALLPAPER)
-WALLPAPER=""
-echo "$CURRENT_WALLPAPER"
 
+if [ "$CURRENT_WALLPAPER" ==  "no wallpapers loaded" ] ; then
+    WALLPAPER=${WALLPAPER_LIST[0]}
+else
+    CURRENT_WALLPAPER=$(readlink -f $CURRENT_WALLPAPER)
 
-INDEX=0
-for i in "${WALLPAPER_LIST[@]}"
-do
-    if [ "$i" == "$CURRENT_WALLPAPER" ] ; then
-        INDEX=$(($INDEX+$1))
-        WALLPAPER=${WALLPAPER_LIST[$INDEX]}
-        break;
-    fi
-    ((INDEX++))
-done
+    INDEX=0
+    for i in "${WALLPAPER_LIST[@]}"
+    do
+        if [ "$i" == "$CURRENT_WALLPAPER" ] ; then
+            INDEX=$(($INDEX+$1))
+            WALLPAPER=${WALLPAPER_LIST[$INDEX]}
+            break;
+        fi
+        ((INDEX++))
+    done
+fi
+
 
 # If a wallpaper is found
 if [[ $WALLPAPER != "" ]] ; then
