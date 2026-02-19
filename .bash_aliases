dfe72c7d85d8021c4b9733d999ca5a22a4d8f776
Pedro P. Alexandre <pedropalexandre@protonmail.com>
2026-02-19 07:23:34 -0300

    Updates my configs

--- .bash_aliases
+++ .bash_aliases
@@ -3,6 +3,7 @@ alias ll='ls -lAhFX'
 alias la='ls -A'
 alias l='ls -CF'
 alias grep='grep --color=auto'
+alias ip='ip --color=auto'
 alias fetch='fastfetch'
 
 function lfcd () {
--- .config/hypr/scripts/change_wallpaper.sh
+++ .config/hypr/scripts/change_wallpaper.sh
@@ -4,12 +4,11 @@ WALLPAPER=""
 WALLPAPER_DIRECTORY=$HOME/.wallpapers
 WALLPAPER_LIST=$(find "$WALLPAPER_DIRECTORY"/* -type f | sort)
 WALLPAPER_LIST=($WALLPAPER_LIST)
-CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
+CURRENT_WALLPAPER=$(readlink -f ~/.config/hypr/scripts/last_wallpaper)
 
 if [ "$CURRENT_WALLPAPER" ==  "no wallpapers loaded" ] ; then
     WALLPAPER=${WALLPAPER_LIST[0]}
 else
-    CURRENT_WALLPAPER=$(readlink -f $CURRENT_WALLPAPER)
 
     INDEX=0
     WALLPAPER_LIST_LENGTH=${#WALLPAPER_LIST[@]}
@@ -34,9 +33,7 @@ fi
 
 # If a wallpaper is found
 if [[ $WALLPAPER != "" ]] ; then
-    hyprctl hyprpaper preload "$WALLPAPER"
     hyprctl hyprpaper wallpaper ",$WALLPAPER"
-    hyprctl hyprpaper unload unused
     rm $HOME/.config/hypr/scripts/last_wallpaper
     ln -s $WALLPAPER $HOME/.config/hypr/scripts/last_wallpaper
 fi
